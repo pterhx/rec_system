f93da649db562874347564972c32dc1f0903068c
Peter Xiao <peterthexiao@gmail.com>
2012-09-10 02:07:02 -0400

    Optimized matrix_factorization. Fixed test_factorization to discard users/animes with few ratings.

--- matrix_factorization.rb
+++ matrix_factorization.rb
@@ -1,58 +1,47 @@
 require 'matrix'
 
-def printMatrix m1
-  m1.to_a.each {|r| puts r.inspect}
+def getError(matrixR, arrP, arrQ, u, i, sizeK)
+  vectorPu = Vector.elements(arrP[u])
+  vectorQi = Vector.elements(arrQ.map{|r| r[i]})
+  matrixR[u,i] - vectorPu.inner_product(vectorQi) 
 end
 
-def getError(matrixR, matrixP, matrixQ, u, i, sizeK)
-  matrixR[u,i] - matrixP.row_vectors[u].inner_product(matrixQ.column_vectors[i]) 
-end
-
-def getRMSE(matrixR, matrixP, matrixQ, sizeK, beta)
+def getRMSE(matrixR, arrP, arrQ, sizeK, beta)
   sum = 0
   matrixR.each_with_index do |r, u, i| 
     next unless r != 0
-    sum += (getError(matrixR, matrixP, matrixQ, u, i, sizeK) ** 2) 
+    sum += (getError(matrixR, arrP, arrQ, u, i, sizeK) ** 2) 
     for k in 0...sizeK
-      sum += ((beta / 2) * ((matrixP[u,k] ** 2) + (matrixQ[k,i] ** 2)))
+      sum += ((beta / 2) * ((arrP[u][k] ** 2) + (arrQ[k][i] ** 2)))
     end
   end
   return sum
 end
 
-def stepQ(matrixP, matrixQ, eui, u, i, k, alpha, beta)
-  matrixQ[k][i] + alpha * (2 * eui * matrixP[u][k] - beta * matrixQ[k][i])
+def stepQ(arrP, arrQ, eui, u, i, k, alpha, beta)
+  arrQ[k][i] + alpha * (2 * eui * arrP[u][k] - beta * arrQ[k][i])
 end
 
-def stepP(matrixP, matrixQ, eui, u, i, k, alpha, beta)
-  matrixP[u][k] + alpha * (2 * eui * matrixQ[k][i]  - beta * matrixP[u][k])
+def stepP(arrP, arrQ, eui, u, i, k, alpha, beta)
+  arrP[u][k] + alpha * (2 * eui * arrQ[k][i]  - beta * arrP[u][k])
 end
 
 def matrix_factorization(matrixR, matrixP0, matrixQ0, sizeK, alpha=0.001, beta=0.02, delta=0.0005)
   puts "Start matrix_factorization"
-  matrixP = matrixP0
-  matrixQ = matrixQ0
-  newR = matrixP * matrixQ
-  newTotalE = getRMSE(matrixR, matrixP, matrixQ, sizeK, beta)
+  arrP, arrQ = matrixP0.to_a, matrixQ0.to_a
+  newTotalE = getRMSE(matrixR, arrP, arrQ, sizeK, beta)
   totalE = 0
   begin
     totalE = newTotalE
-    newPArr = matrixP.to_a
-    newQArr = matrixQ.to_a
     matrixR.each_with_index do |r, u, i|
       next unless r != 0
-      eui = getError(matrixR, matrixP, matrixQ, u, i, sizeK)
+      eui = getError(matrixR, arrP, arrQ, u, i, sizeK)
       for k in 0...sizeK
-        newPArr[u][k] = stepP(newPArr, newQArr, eui, u, i, k, alpha, beta)
-        newQArr[k][i] = stepQ(newPArr, newQArr, eui, u, i, k, alpha, beta)
+        arrP[u][k] = stepP(arrP, arrQ, eui, u, i, k, alpha, beta)
+        arrQ[k][i] = stepQ(arrP, arrQ, eui, u, i, k, alpha, beta)
       end
     end
-
-    matrixP = Matrix.rows(newPArr)
-    matrixQ = Matrix.rows(newQArr)
-    newR = matrixP * matrixQ
-    newTotalE = getRMSE(matrixR, matrixP, matrixQ, sizeK, beta)
-    # puts "Delta: #{totalE - newTotalE}"
+    newTotalE = getRMSE(matrixR, arrP, arrQ, sizeK, beta)
   end while totalE - newTotalE > delta
-  return newR
+  return Matrix.rows(arrP) * Matrix.rows(arrQ)
 end
--- test_factorization.rb
+++ test_factorization.rb
@@ -4,29 +4,50 @@ require_relative 'matrix_factorization'
 require 'matrix'
 require 'benchmark'
 
-def printMatrix m1
-  m1.to_a.each {|r| puts r.inspect}
-end
-
-DB = Sequel.connect('jdbc:sqlite:MAL.db')
-
+# Constants
 NUM_USERS = 30
 NUM_ANIMES = 30
 NUM_REMOVED = 10
 K = 10
+MIN_ANIMES = 50
+MIN_USERS = 100
 
-users = DB[:users].first(NUM_USERS).map{|r| r[:id]}
-animes = DB[:animes].first(NUM_ANIMES).map{|r| r[:id]}
+# Retrieve users, animes, and ratings
+DB = Sequel.connect('jdbc:sqlite:MAL.db')
 
-ratingsDS = DB[:ratings].filter([:user_id, users], [:anime_id, animes])
-ratings = ratingsDS.map{|r| [r[:user_id], r[:anime_id], r[:score]]}
+usersDS = DB[:users]
+animesDS = DB[:animes]
+ratingsDS = DB[:ratings]
 
-arrR = Array.new(NUM_USERS) {Array.new(NUM_ANIMES) {0}}
+filteredAnimesDS = animesDS.join(:ratings, :anime_id => :id).group_and_count(:anime_id).having('count > ?', MIN_USERS).first(NUM_ANIMES)
+filteredUsersDS = usersDS.join(:ratings, :user_id => :id).group_and_count(:user_id).having('count > ?', MIN_ANIMES).first(NUM_USERS)
 
-ratingsDS.each do |row|
-  arrR[row[:user_id] - 1][row[:anime_id] - 1] = row[:score]
+users = filteredUsersDS.map{|r| r[:user_id]}
+animes = filteredAnimesDS.map{|r| r[:anime_id]}
+filteredRatingsDS = ratingsDS.filter([:user_id, users], [:anime_id, animes])
+ratings = filteredRatingsDS.map{|r| [r[:user_id], r[:anime_id], r[:score]]}
+
+# Build look up hashes to convert anime_id
+userLookUp = Hash.new
+animeLookUp = Hash.new
+
+users.each_index do |i|
+  userLookUp[users[i]] = i
+end
+animes.each_index do |i|
+  animeLookUp[animes[i]] = i
 end
 
+# Fill in R
+arrR = Array.new(NUM_USERS) {Array.new(NUM_ANIMES) {0}}
+
+filteredRatingsDS.each do |row|
+  userIndex = userLookUp[row[:user_id]]
+  animeIndex = animeLookUp[row[:anime_id]]
+  arrR[userIndex][animeIndex] = row[:score]
+end
+
+# Randomly remove elements
 removed = Array.new(NUM_REMOVED)
 
 for i in 0...NUM_REMOVED
@@ -35,12 +56,12 @@ for i in 0...NUM_REMOVED
     rating = ratings.sample
   end
   removed[i] = rating
-  arrR[rating[0] - 1][rating[1] - 1] = 0
+  arrR[userLookUp[rating[0]]][animeLookUp[rating[1]]] = 0
 end
 
-arrP0 = Array.new(NUM_USERS) {Array.new(K) {0.5}}
+arrP0 = Array.new(NUM_USERS) {Array.new(K) {Random.rand}}
 matrixP0 = Matrix.rows(arrP0)
-arrQ0 = Array.new(K) {Array.new(NUM_ANIMES) {0.5}}
+arrQ0 = Array.new(K) {Array.new(NUM_ANIMES) {Random.rand}}
 matrixQ0 = Matrix.rows(arrQ0)
 matrixR = Matrix.rows(arrR)
 
@@ -48,18 +69,17 @@ estimatedR = nil
 time = Benchmark.realtime do
   estimatedR = matrix_factorization(matrixR, matrixP0, matrixQ0, K)
 end
+
+
 puts "============= RESULTS ============="
-puts "Factorization took #{time*1000} milliseconds"
-puts "================ R ================"
-printMatrix matrixR
-puts "=============== eR ================"
-printMatrix estimatedR
+puts "Factorization took #{time} seconds"
+
 puts "============== Stats =============="
 diff = 0
 removed.each do |rating|
   user, anime, score = rating
   puts "User: #{user}, Anime: #{anime}"
-  estimatedScore = estimatedR[user-1, anime-1]
+  estimatedScore = estimatedR[userLookUp[user], animeLookUp[anime]]
   puts "Estimated:\t #{estimatedScore}"
   puts "Actual: \t #{score}"
   diff += (estimatedScore - score).abs
