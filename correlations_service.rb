dc7cd7d1d2bf889d7a91b2e331a47328e9cefe63
Peter Xiao <peterthexiao@gmail.com>
2012-09-20 01:35:44 -0400

    Fixed getNeighbors function a bit.

--- correlations_service.rb
+++ correlations_service.rb
@@ -7,6 +7,7 @@ DB.create_table? :correlations do
   foreign_key :anime1_id, :animes
   foreign_key :anime2_id, :animes 
   float :correlation
+  integer :num_users
   unique [:anime1_id, :anime2_id]
 end
 
@@ -14,12 +15,11 @@ ratingsDS = DB[:ratings]
 animesDS = DB[:animes]
 correlationsDS = DB[:correlations]
 
-firstAnimeId = correlationsDS.first[:anime1_id]
+firstAnimeId = correlationsDS.order(:id).last[:anime1_id]
 lastAnimeId = animesDS.order(:id).last[:id]
 
 for anime in firstAnimeId..lastAnimeId
   for otherAnime in (anime + 1)..lastAnimeId
-    next unless anime != otherAnime
     next unless correlationsDS.filter(:anime1_id => anime, :anime2_id => otherAnime).empty?
     usersSeenAnime2 = ratingsDS.select(:user_id).filter(:anime_id => otherAnime)
     users = ratingsDS.filter(:anime_id=> anime,:user_id => usersSeenAnime2).select(:user_id)
@@ -33,7 +33,7 @@ for anime in firstAnimeId..lastAnimeId
     correlation = GSL::Stats::correlation(GSL::Vector.alloc(animeRatings),GSL::Vector.alloc(otherAnimeRatings))
     next if correlation.nan?
     puts "corr(#{anime}, #{otherAnime}) = #{correlation}"
-    correlationsDS.insert(:anime1_id => anime, :anime2_id => otherAnime, :correlation => correlation)
-    correlationsDS.insert(:anime1_id => otherAnime, :anime2_id => anime, :correlation => correlation)
+    correlationsDS.insert(:anime1_id => anime, :anime2_id => otherAnime, :correlation => correlation, :num_users => users.count)
+    correlationsDS.insert(:anime1_id => otherAnime, :anime2_id => anime, :correlation => correlation, :num_users => users.count)
   end
 end
--- nearest_neighbor.rb
+++ nearest_neighbor.rb
@@ -7,17 +7,35 @@ BETA = 500
 K = 15
 
 def regularize(correlation, numUsers)
-  ((numUsers * correlation) / (numUsers + BE
+  ((numUsers * correlation) / (numUsers + BETA))
 end
 
-def getNeighbors(animeId)
+def getNeighbors(userId, animeId)
+  # grab the user's animes
+  ratingsDS= DB[:ratings];
+  userAnimes = ratingsDS.filter(:user_id => userId).map {|r| r[:anime_id]}
+  # grab all the correlations for animdId
   correlationsDS = DB[:correlations]
-  correlations = correlationsDS.filter(:anime1_id => animeId).map {|r| r[:correlation]}
+  filteredCorrelationsDS = correlationsDS.filter(:anime1_id => animeId) 
+  correlations = filteredCorrelationsDS.map {|r| r}
+  # keep animes seen by user
+  correlations.select! {|correlation| userAnimes.include? correlation[:anime2_id] 
+  onlyCorrelations = correlations.map {|correlation| correlation[:correlation]}
   num = [K, correlations.size].min
-  GSL::Vector.alloc(correlations).sort_largest(num).map{|correlation| regularize(correlation, )}
+  # keep largest num indices
+  neighborIndices = GSL::Vector.alloc(onlyCorrelations).sort_largest_index(num);
+  return neigbhorIndices.map do |index|
+    correlationData = correlations[index]
+    correlation = correlationData[:correlation]
+    numUsers = correlationData[:num_users]
+    otherAnimeId = correlationData[:anime2_id]
+    {:correlation => regularize(correlation, numUsers), :animdId => otherAnimeId}
+  end
 end
 
-def predict()
+def predict(userId, animeId)
+  neighbors = getNeighbors(animeId)
+  
 end
 
 def recommend()
